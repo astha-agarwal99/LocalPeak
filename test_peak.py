1dc951b930eaac40875dfa6531a483267a46586e
astha-agarwal99 <astha99agarwal@gmail.com>
2020-02-18 16:14:13 +0530

    Add:-No element has no peak

---
+++ test_peak.py
@@ -0,0 +1,13 @@
+import unittest
+def peak(lst):
+    return [ ]
+
+
+
+class PeakTest(unittest.TestCase):
+    def No_element_has_no_peak(self):
+        self.assertEqual([ ], peak())
+
+
+if __name__ == '__main__':
+    unittest.main()
