47b785958c8dfb7a5a8ea7ef708946f1181d412e
astha-agarwal99 <astha99agarwal@gmail.com>
2020-02-18 16:37:56 +0530

    Add:Peak is the greatest element if list contains 2 elements

--- test_peak.py
+++ test_peak.py
@@ -1,28 +1,28 @@
 import unittest
-def peak(lst):
-    peak_list=[ ]
-    if(len(lst)==1):
-        peak_list.append(lst[0])
-    if(len(lst)>1):
-        if lst[0]>lst[1]:
-            peak_list.append(lst[0])
-        else:
-            peak_list.append(lst[1])
-    return peak_list
 
+
+def peak(lst):
+    peak_num = None
+    if len(lst) == 1:
+        peak_num = lst[0]
+    if len(lst) > 1:
+        if lst[0] > lst[1]:
+            peak_num = lst[0]
+        else:
+            peak_num = lst[1]
+    return peak_num
 
 
 class PeakTest(unittest.TestCase):
 
     def test_No_element_has_no_peak(self):
-        self.assertEqual([ ], peak([]))
-
+        self.assertEqual(None, peak([]))
 
     def test_Single_element_has_itself_as_a_peak(self):
-        self.assertEqual([1 ], peak([1]))
+        self.assertEqual(1, peak([1]))
 
     def test_Two_elements_has_greatest_as_a_peak(self):
-        self.assertEqual([2], peak([1,2]))
+        self.assertEqual(2, peak([1, 2]))
 
 
 if __name__ == '__main__':
